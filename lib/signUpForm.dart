192474e19744a94f5a5cabb47317c18141091258
avivv <>
2021-08-08 17:53:04 +0300

    first push v0.1

--- lib/signUpForm.dart
+++ lib/signUpForm.dart
@@ -14,56 +14,55 @@ class _signUpFormState extends State<signUpForm> {
   Widget build(BuildContext context) {
     return Scaffold(
         // backgroundColor: Color(0xFF63BA66),
-        backgroundColor: Color(0xFFFFFFFF),
+        // backgroundColor: Color(0xFFFFFFFF),
         body: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.all(25.0),
-            child: Center(
-              child: Column(
-                children: [
-                  SizedBox(height: 40),
-                  Icon(Icons.business_center, color: Colors.green, size: 135),
-                  SizedBox(
-                    height: 13,
-                  ),
-                  Text("שלום לך",
-                      style:
-                          TextStyle(fontSize: 45, fontWeight: FontWeight.bold)),
-                  Text("בחר סניף והמשך",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.grey,
-                      )),
-                  SizedBox(height: 20),
-                  Container(
-                    child: Directionality(
-                      textDirection: TextDirection.rtl,
-                      child: TextFormField(
-                          enableSuggestions: false,
-                          autocorrect: false,
-                          obscureText: true,
-                          style: TextStyle(
-                              color: Theme.of(context).primaryColor,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 25),
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            prefixIcon: Icon(
-                              FontAwesomeIcons.lock,
-                              size: 30,
-                            ),
-                            labelText: "סיסמא",
-                            labelStyle: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.w800),
-                          )),
-                    ),
-                  ),
-                ],
+      child: Padding(
+        padding: EdgeInsets.all(25.0),
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 40),
+              Icon(Icons.business_center, color: Colors.green, size: 135),
+              SizedBox(
+                height: 13,
               ),
-            ),
+              Text("שלום לך",
+                  style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold)),
+              Text("בחר סניף והמשך",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.grey,
+                  )),
+              SizedBox(height: 20),
+              Container(
+                child: Directionality(
+                  textDirection: TextDirection.rtl,
+                  child: TextFormField(
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      obscureText: true,
+                      style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25),
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: Icon(
+                          FontAwesomeIcons.lock,
+                          size: 30,
+                        ),
+                        labelText: "סיסמא",
+                        labelStyle: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w800),
+                      )),
+                ),
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    ));
   }
 }
 
